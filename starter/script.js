33cf1df2e442ddb6e7708734c2d6593580b8f852
deividas strole <deiwis@yahoo.com>
2025-01-07 17:55:28 -0800

    w

--- starter/script.js
+++ starter/script.js
@@ -466,5 +466,21 @@ console.log(
 
 console.log('********************>190<***************************');
 
-setTimeout(() => console.log('Here is your pizza'), 3000);
+const ingredients = ['olives', 'spinach'];
+const pizzaTimer = setTimeout(
+  (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2}`),
+  3000,
+  ...ingredients
+);
 console.log('Waiting....');
+
+if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
+
+// set timeout
+
+// setInterval(function () {
+//   const now = new Date();
+//   console.log(now);
+// }, 1000);
+
+console.log('********************>191<***************************');
